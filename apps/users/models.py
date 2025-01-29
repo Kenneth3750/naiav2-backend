4bcd1895a32cafb69d1eb41d4bf85ddd25944179
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-01-29 10:50:26 -0500

    .

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     family_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     photo_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
