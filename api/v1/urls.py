706f3aa92c6951541253ff5874d22cd45ad5cc46
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-02-11 09:23:53 -0500

    Update urls.py

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 
@@ -6,4 +7,7 @@ urlpatterns = [
     path('users/', include('api.v1.users.urls')),
     path('token/', include('api.v1.token.urls')),
     path('api-auth/', include('rest_framework.urls')), 
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
