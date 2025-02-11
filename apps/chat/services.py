365040ad55491aae12fdb087ac55f69082a33700
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-02-11 10:05:55 -0500

    Update services.py
    This is just a test version

---
+++ apps/chat/services.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from services.llm import LLMService
+import os
+
+def get_rain_probability(location):
+    import random
+    random_temp = random.randint(0, 100)
+    print("Función get_rain_probability ejecutada")
+    return "The probability of rain in {} is {}%".format(location, random_temp)
+
+
+
+class ChatService():
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("open_ai")
+
+    def generate_response(self, messages, username, user_input, image_url, thread_id, assistant_id):
+        available_tools = {
+            "get_rain_probability": get_rain_probability
+        }
+        llm_service = LLMService(self.api_key, available_tools, thread_id, assistant_id)
+        llm_service.init_conversation(messages, username)
+        return llm_service.generate_response(user_input, image_url)
