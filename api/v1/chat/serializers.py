786bd268625e61d3391649a4128c7b65446127b2
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-02-27 14:05:13 -0500

    Update serializers.py
    Change serializer variables

--- api/v1/chat/serializers.py
+++ api/v1/chat/serializers.py
@@ -4,5 +4,8 @@ from rest_framework import serializers
 class ChatSerializer(serializers.Serializer):
     user_id = serializers.IntegerField(allow_null=False, required=True)
     user_input = serializers.CharField(allow_blank=False, required=False)
-    thread_id = serializers.CharField(allow_blank=True, required=True)
-    assistant_id = serializers.CharField(allow_blank=False, required=True)
+    role_id = serializers.CharField(allow_blank=False, required=True)
+
+class ChatMessagesSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField(allow_null=False, required=True)
+    role_id = serializers.IntegerField(allow_null=False, required=True)
