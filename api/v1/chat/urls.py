71c1eb57cf80aa03b1c892b273bd7f56d5af7a48
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-02-27 14:05:55 -0500

    Add chat messages functionality

--- api/v1/chat/urls.py
+++ api/v1/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import Chat
+from .views import Chat, ChatMessages
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', Chat.as_view(), name='chat'),
+    path('messages/', ChatMessages.as_view(), name='chat-messages'),
 ]
--- api/v1/chat/views.py
+++ api/v1/chat/views.py
@@ -9,9 +9,10 @@ from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.openapi import OpenApiResponse
-from .serializers import ChatSerializer
+from .serializers import ChatSerializer, ChatMessagesSerializer
 from dotenv import load_dotenv
 import os
+import time
 load_dotenv()
 
 
@@ -19,7 +20,6 @@ class Chat(APIView):
     permission_classes = [IsAuthenticated]  
     def __init__(self):
         self.chat_service = ChatService()
-        self.image_url = os.getenv("bucket_url")
 
 
     def post(self, request):
@@ -28,29 +28,49 @@ class Chat(APIView):
             if serializer.is_valid():
                 user_id = serializer.validated_data['user_id']
                 user_input = serializer.validated_data['user_input']
-                thread_id = serializer.validated_data['thread_id']
-                assistant_id = serializer.validated_data['assistant_id']
-
+                role_id = serializer.validated_data['role_id']
+                start_time = time.time()
                 response = self.chat_service.generate_response(
                     username="John Doe",
                     user_input=user_input,
                     user_id=user_id,
-                    thread_id=thread_id,
-                    assistant_id=assistant_id,
+                    role_id=role_id,
                 )
+                end_time = time.time()
+                print("Tiempo de respuesta: ", end_time - start_time)
                 return Response(response, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
-    def delete(self, request):
+
+class ChatMessages(APIView):
+    permission_classes = [IsAuthenticated]
+    def __init__(self):
+        self.chat_service = ChatService()
+
+    def post(self, request):
         try:
-            thread_id = request.query_params.get('thread_id')
-            if not thread_id:
-                return Response({"error": "Thread ID is required"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            self.chat_service.delete_current_thread(thread_id)
-            return Response("Thread deleted successfully", status=status.HTTP_200_OK)
+            serializer = ChatMessagesSerializer(data=request.data)
+            if serializer.is_valid():
+                user_id = serializer.validated_data['user_id']
+                role_id = serializer.validated_data['role_id']
+                self.chat_service.save_current_conversation(user_id, role_id)
+                self.chat_service.delete_current_conversation(user_id, role_id)
+                return Response({"message": "Conversación guardada"}, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)   
+        
+    def get(self, request):
+        try:
+            user_id = request.query_params.get('user_id')
+            role_id = request.query_params.get('role_id')
+            if not user_id or not role_id:
+                return Response({"error": "user_id and role_id are required"}, status=status.HTTP_400_BAD_REQUEST)
+            messages = self.chat_service.get_conversation(user_id, role_id)
+            return Response(messages, status=status.HTTP_200_OK)
         except Exception as e:
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
