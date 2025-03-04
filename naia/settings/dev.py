82e18c9f6ed7beb64779db8e41b2c16ea6163789
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-03-04 14:40:50 -0500

    Update dev.py

--- naia/settings/dev.py
+++ naia/settings/dev.py
@@ -26,8 +26,12 @@ MIDDLEWARE = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('db_name'),
+        'USER': os.getenv('db_user'),
+        'PASSWORD': os.getenv('db_password'),
+        'HOST': os.getenv('db_host'),   
+        'PORT': '3306',     
     }
 }
 
