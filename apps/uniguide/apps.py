3f0a12ea4430e7ebced0ce128cb6c7271d56e1c9
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-05-19 15:39:37 -0500

    Create Uni Guide role

---
+++ apps/uniguide/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UniguideConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.uniguide"
--- naia/settings/base.py
+++ naia/settings/base.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
     'apps.roles',
     'apps.status',
     'apps.researcher',
+    'apps.uniguide',
 ]
 
 
