51f4f7e245dfaefe79618f4092abecbd6fccbc2c
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2025-06-25 09:11:38 -0500

    Update serializers.py

--- api/v1/users/serializers.py
+++ api/v1/users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from apps.users.models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
+    photo_url = serializers.CharField(allow_blank=True, required=False)
+    
     class Meta:
         model = User
         fields = ('name', 'family_name', 'email', 'photo_url')
-
